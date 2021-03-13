d437aaafd220bfc68b68eeb88dba0be9f957edd3
cloworm <chloe.echikson@gmail.com>
2021-03-12 21:14:38 -0500

    added typescript, jest, eslint, and ci

---
+++ tests/index.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Index from '../pages/Index'
+
+describe('Index', () => {
+  test('first test', () => {
+    render(<Index />)
+  })
+})
