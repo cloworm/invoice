b7aa3050ae431f2c0149c7ed564ec34993c32737
cloworm <chloe.echikson@gmail.com>
2022-10-23 17:59:16 -0400

    added conditional theming to status badge

--- components/status-badge.tsx
+++ components/status-badge.tsx
@@ -1,24 +1,61 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components';
 
-const Badge = styled.div`
+interface ThemeProps {
+  status: string
+}
+
+const Badge = styled.div<ThemeProps>`
   width: 104px;
   height: 40px;
   display: flex;
   align-items: center;
   justify-content: center;
-  background: rgba(223, 227, 250, 5.71%);
+  background: ${(props) => {
+    switch (props.status) {
+      case "paid":
+        return 'rgba(51, 214, 159, 5.71%)'
+      
+      case 'pending':
+        return 'rgba(255, 143, 0, 5.71%)'
+    
+      default:
+        return 'rgba(223, 227, 250, 5.71%)'
+    }
+  }};
   border-radius: 6px;
   text-transform: capitalize;
-  color: #DFE3FA;
+  color: ${(props) => {
+    switch (props.status) {
+      case "paid":
+        return '#33D69F';
+      
+      case "pending":
+        return '#FF8F00'
+      
+      default:
+        return '#DFE3FA';
+    }
+  }} 
 `
 
-const Dot = styled.div`
+const Dot = styled.div<ThemeProps>`
   height: 8px;
   width: 8px;
   border-radius: 4px;
-  background-color: #DFE3FA;
   margin-right: 8px;
+  background-color: ${(props) => {
+    switch (props.status) {
+      case "paid":
+        return '#33D69F';
+              
+      case "pending":
+        return '#FF8F00'
+      
+      default:
+        return '#8d91a8';
+    }
+  }} 
 `
 
 interface Props {
@@ -27,8 +64,8 @@ interface Props {
 
 const StatusBadge: FunctionComponent<Props> = ({ status }) => {
   return (
-    <Badge>
-      <Dot />
+    <Badge status={status}>
+      <Dot status={status} />
       <h4>
         {status}
       </h4>
