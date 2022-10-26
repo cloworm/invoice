f9fcaa77c9b1591235aabd290185e309f06127dd
cloworm <chloe.echikson@gmail.com>
2022-10-25 20:38:04 -0400

    created button component with variants

--- components/list-header.tsx
+++ components/list-header.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useContext } from 'react'
 import styled from 'styled-components'
 import { InvoiceContext, Actions } from '../state/invoice.state'
 import MultiselectDropdown from '@/components/multiselect-dropdown'
+import Button from '@/components/button'
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -10,6 +11,12 @@ const HeaderContainer = styled.div`
 
 const Title = styled.h1`
   padding-bottom: 8px;
+`
+
+const ListActions = styled.div`
+  display: flex;
+  column-gap: 40px;
+  align-items: center;
 `
 
 const ListHeader: FunctionComponent = () => {
@@ -32,9 +39,10 @@ const ListHeader: FunctionComponent = () => {
           { invoices.length === 0 ? 'No invoices' : invoices.length + ' Invoices'}
         </p>
       </div>
-      <div>
+      <ListActions>
         <MultiselectDropdown label="Filter by status" options={options} onChange={handleSelect} selected={filter} />
-      </div>
+        <Button label="New Invoice" variant="icon-primary" />
+      </ListActions>
     </HeaderContainer>
   )
 }
