36605c9be2d2b5381d7b567222c1fa5e15506571
cloworm <chloe.echikson@gmail.com>
2022-10-25 22:02:37 -0400

    fetching invoice by id on record page

--- pages/invoices/[id].tsx
+++ pages/invoices/[id].tsx
@@ -1,12 +1,74 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useContext, useState } from 'react'
 import styled from 'styled-components'
 import { useRouter } from 'next/router'
+import StatusBadge from '@/components/status-badge'
+import Button from '@/components/button'
+import { InvoiceContext, Invoice } from '../../state/invoice.state'
+
+const Header = styled.div`
+  background-color: #1E2139;
+  padding: 20px 32px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 8px;
+  margin-bottom: 24px;
+`
+
+const FlexItemsCenter = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  column-gap: 8px;
+`
+
+const StatusLabel = styled.p`
+  padding-right: 16px;
+`
+
+const Details = styled.div`
+  background-color: #1E2139;
+  padding: 48px;
+  border-radius: 8px;
+`
+
+const Hash = styled.span`
+  color: #888EB0;
+`
 
 const Invoice: FunctionComponent = () => {
+  const [invoice, setInvoice] = useState<Invoice>()
+  const { state: { invoices } } = useContext(InvoiceContext)
   const router = useRouter()
   const { id } = router.query
+
+  useEffect(() => {
+    const currentInvoice = invoices.find((invoice) => invoice.id === id)
+    setInvoice(currentInvoice)
+  }, [id, invoices])
+
+  if (!invoice) return <div></div>
+
   return (
-    <div>Invoice Id: {id}</div>
+    <div>
+      <Header>
+        <FlexItemsCenter>
+          <StatusLabel>Status</StatusLabel>
+          <StatusBadge status={invoice.status} />
+        </FlexItemsCenter>
+        <ButtonsContainer>
+          <Button variant="tertiary" label="Edit" />
+          <Button variant="error" label="Delete" />
+          <Button variant="primary" label="Mark as Paid" />
+        </ButtonsContainer>
+      </Header>
+      <Details>
+        <h3><Hash>#</Hash>{id}</h3>
+      </Details>
+    </div>
   )
 }
 
--- state/invoice.state.tsx
+++ state/invoice.state.tsx
@@ -1,7 +1,7 @@
 import React, { Dispatch, createContext, useReducer, FunctionComponent, useMemo } from 'react'
 import data from '../public/data.json'
 
-interface Invoice {
+export interface Invoice {
   id: string
   paymentDue: string
   clientName: string
